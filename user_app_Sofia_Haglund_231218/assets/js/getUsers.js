c86da9efef217e35aa404e899736731bdd140685
sofiahag <sofia.h4glund@gmail.com>
2023-12-18 15:27:39 +0100

    first commit, add simple user display app

---
+++ user_app_Sofia_Haglund_231218/assets/js/getUsers.js
@@ -0,0 +1,13 @@
+
+const USER_LIST = [];
+
+//Hämta datan från API:n
+export const getUsers = async () => {
+    const result = await fetch('https://reqres.in/api/users/');
+    const data = await result.json();
+    for (const [key, val] of Object.entries(data)){
+        USER_LIST.push(val);
+    }
+    return USER_LIST;
+}
+
